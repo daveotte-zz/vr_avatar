b6f188a5fa8b49933ce70c5743aa04e3d36c97f5
daveotte <otte.dave@gmail.com>
2016-08-16 12:55:24 -0700

    first commit

---
+++ fbx_data_miner.py
@@ -0,0 +1,16 @@
+
+import sys
+sys.path.append('/home/daveotte/work/ImportScene')
+
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+import site
+
+
+for i in (sys.path):
+	print i
+
+import DisplayGlobalSettings
+
+
